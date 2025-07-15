f75abf8ff636788eceb10ae41e11399d7444ef47
gface <89259112970@mail.ru>
2025-07-15 23:12:32 +0300

    add some fix

--- src/main/java/com/example/bankcards/util/CardMapper.java
+++ src/main/java/com/example/bankcards/util/CardMapper.java
@@ -15,27 +15,4 @@ public class CardMapper {
                 card.getBalance()
         );
     }
-}
-
-// Новый маппер для User -> UserDto
-package com.example.bankcards.util;
-
-import com.example.bankcards.dto.UserDto;
-import com.example.bankcards.entity.User;
-import java.util.stream.Collectors;
-
-public class UserMapper {
-    public static UserDto toDto(User user) {
-        UserDto dto = new UserDto();
-        dto.setId(user.getId());
-        dto.setUsername(user.getUsername());
-        dto.setEmail(user.getEmail());
-        // Добавим список ролей (имена)
-        if (user.getRoles() != null) {
-            dto.setRoles(user.getRoles().stream()
-                .map(role -> role.getName().name())
-                .collect(Collectors.toList()));
-        }
-        return dto;
-    }
 } 
