31c654bc83a74ae32e3437d8723dfb722dbb6086
gface <89259112970@mail.ru>
2025-07-21 12:20:07 +0300

    add final some fix

--- src/main/java/com/example/bankcards/security/SecurityConfig.java
+++ src/main/java/com/example/bankcards/security/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
